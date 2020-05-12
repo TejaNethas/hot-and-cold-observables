52b4f906ffcd92ab292031c558de66f109d4820b
Teja_Nethas <saitejau12@gmail.com>
2020-05-12 23:07:24 +0530

    hot-observable
    we moved our data producer out of the observable.

---
+++ hot-observable.ts
@@ -0,0 +1,17 @@
+import * as Rx from "rxjs";
+
+const random = Math.random()
+
+const observable = Rx.Observable.create((observer) => {
+    observer.next(random);
+});
+
+// subscription 1
+observable.subscribe((data) => {
+  console.log(data); // 0.11208711666917925 (random number)
+});
+
+// subscription 2
+observable.subscribe((data) => {
+   console.log(data); // 0.11208711666917925 (random number)
+});
